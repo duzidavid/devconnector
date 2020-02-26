1ace2d5340c5c6438a41c53d3501fbc84ade0737
duzidavid <david.duzi@teliacompany.com>
2020-02-26 19:50:31 +0100

    Adding Delete profilr & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,4 +154,28 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+//@route    DELETE api/profile
+//@desc     Delete profile, user & posts
+//@acces    Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //@todo - remove users posts
+
+    //Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res
+        .status(400)
+        .json({ errors: { msg: 'Profile does not exists' } });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
